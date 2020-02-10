85bd2c5505f0d2260cea29b66db1e67be321db52
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-10 22:42:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -30,17 +30,7 @@ def map_to_square(array)
 return array 
 end 
 
-def reduce_to_total(array)
-  index = 0 
-  total = 0 
-while index < array.length 
-  total += array[index]
-  index += 1 
-end 
-return total 
-end 
-
-def reduce_to_total(array,starting_total)
+def reduce_to_total(array,starting_total = 0)
   index = 0 
   total = starting_total
 while index < array.length 
